6224c5833082102d314b1e60386b846d41a37230
MelodyRocks <johnsonjorden49@gmail.com>
2025-07-18 16:53:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,79 +1,77 @@
 import asyncio, aiofiles
-import logging, coloredlogs
+from asyncio.log import logger
+from asyncore import loop
+import logging,coloredlogs
+from radar import TWCRadarCollector
 import os
 from datetime import datetime
-
 import RecordTasks
 
-# Set up logger
 l = logging.getLogger(__name__)
 coloredlogs.install(logger=l)
 
-ascii_art = """
-         ############                              
-     ##################                           
-   ######################                         
-Ss  ########################                        
- #############  ###########                       
-############     ###########                      
-############################                      
-#############################                     
-#############################                     
-#############################                     
-###########       ###########                     
- ####################################             
-  #######################################         
-   ########################################       
-     ########################################     
-         #####################################    
-          ##################       ############   
-     #    #################         ############  
-    #######################          ###########  
-    #####################            ###########  
-    #####################           ############  
-    #####################           ############  
-          ###############         #############   
-          ####################################    
-          ###################################     
-          #################################       
-           #############################
-"""
+useRadarServer = True
 
-l.info(ascii_art)
 l.info("Starting i2RecordCollector")
-l.info("Made for the 9D crew froked from i2ME")
+l.info("Developed by mewtek32, Floppaa, Goldblaze, and needlenose")
 
 async def createTemp():
-    """Creates necessary files & directories for the message encoder to work properly."""
-    if not os.path.exists('./.temp/'):
-        l.info("Creating necessary directories & files...")
-        os.makedirs('./.temp/tiles/output', exist_ok=True)
+    """ Used on a first time run, creates necessary files & directories for the message encoder to work properly. """
+    if not (os.path.exists('./.temp/')):
+        l.info("Creating necessary directories & files..")
+        os.mkdir('./.temp')
+        
+        # Used for the record generator
+        os.mkdir('./.temp/tiles/')
+        os.mkdir('./.temp/tiles/output/')
 
+
+        os.mkdir('./.temp/output')
+
+
+        # Create msgId file for bit.py
         async with aiofiles.open('./.temp/msgId.txt', 'w') as msgId:
             await msgId.write('410080515')
+            await msgId.close()
     else:
-        l.debug(".temp directory already exists")
+        l.debug(".temp file exists")
+        return
+
 
 async def main():
     await createTemp()
 
-    tasks = [
-        asyncio.create_task(RecordTasks.alertsTask()),
-        asyncio.create_task(RecordTasks.coTask()),
-        asyncio.create_task(RecordTasks.hfTask()),
-        asyncio.create_task(RecordTasks.dfTask()),
-        asyncio.create_task(RecordTasks.aqTask()),
-        asyncio.create_task(RecordTasks.aptTask()),
-        asyncio.create_task(RecordTasks.apTask()),
-        asyncio.create_task(RecordTasks.brTask()),
-        asyncio.create_task(RecordTasks.hcTask()),
-        asyncio.create_task(RecordTasks.maTask()),
-        asyncio.create_task(RecordTasks.pTask()),
-        asyncio.create_task(RecordTasks.tTask()),
-        asyncio.create_task(RecordTasks.wnTask()),
-    ]
+    mosaicTask = asyncio.create_task(RecordTasks.updateMosaicTask())
+    satradTask = asyncio.create_task(RecordTasks.updateSatradTask())
+    alertsTask = asyncio.create_task(RecordTasks.alertsTask())
+    coTask = asyncio.create_task(RecordTasks.coTask())
+    hfTask = asyncio.create_task(RecordTasks.hfTask())
+    dfTask = asyncio.create_task(RecordTasks.dfTask())
+    aqTask = asyncio.create_task(RecordTasks.aqTask())
+    aptTask = asyncio.create_task(RecordTasks.aptTask())
+    apTask = asyncio.create_task(RecordTasks.apTask())
+    brTask = asyncio.create_task(RecordTasks.brTask())
+    hcTask = asyncio.create_task(RecordTasks.hcTask())
+    maTask = asyncio.create_task(RecordTasks.maTask())
+    pTask = asyncio.create_task(RecordTasks.pTask())
+    tTask = asyncio.create_task(RecordTasks.tTask())
+    wnTask = asyncio.create_task(RecordTasks.wnTask())
 
-    await asyncio.gather(*tasks)
+    # In theory, these should all run concurrently without problems
+    await alertsTask
+    await coTask
+    await hfTask
+    await dfTask
+    await aqTask
+    await aptTask
+    await apTask
+    await brTask
+    await hcTask
+    await maTask
+    await pTask
+    await tTask
+    await wnTask
+
 
 if __name__ == "__main__":
     asyncio.run(main())
